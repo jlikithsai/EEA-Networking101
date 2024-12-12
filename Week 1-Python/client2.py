c98571ba6f896e8b816dc05a8987e2fa9a031d5d
AmanMaloo <amanmaloo54@gmail.com>
2024-12-12 21:45:35 +0530

    multi threading

--- Week 1-Python/client2.py
+++ Week 1-Python/client2.py
@@ -2,7 +2,7 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('127.0.0.1', 8080))
-    s.sendall(b"Cal 7-9")
+    s.sendall(b"Cal 8*109")
     data = s.recv(1024)
 
-print('Received', data.decode())
+print('received', data.decode())
